9861d75074bb3a55a4e0942e72f5bd2cc7789d8f
Alena Nemtsova <jelenatbs@gmail.com>
2023-12-15 21:32:16 +0300

    add bonus-task lesson04

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
 'use strict';
 
-// Переменная lang может принимать 2 значения: 'ru' 'en'
-let lang = 'ru';
+function myFunc(arg) {
+    if (typeof arg === 'string') {
+        let argTrimmed = arg.trim();
+        if (argTrimmed.length > 30) {
+            argTrimmed = (arg.slice(0, 30) + '...')
+            console.log(argTrimmed);
+        }
+    } else alert('Это не строка');
+}
 
-//Написать условия, при которых в зависимости от значения lang будут выводиться дни недели на русском или английском языке.
-//через if
-if (lang === 'ru') {
-    console.log('Пн, Вт, Ср, Чтв, Птн, Сб, Вс');
-} else if (lang === 'eng') {
-    console.log('Mon, Tue, Wed, Thur, Fri, Sat, San');
-};
-
-//через switch-case
-switch (lang) {
-    case 'ru':
-        console.log('Пн, Вт, Ср, Чтв, Птн, Сб, Вс');
-        break
-    case 'eng':
-        console.log('Mon, Tue, Wed, Thur, Fri, Sat, San');
-        break
-};
-
-//через многомерный массив без ифов и switch
-let langArray = [
-    ['Пн', 'Вт', 'Ср', 'Чтв', 'Птн', 'Сб', 'Вс'],
-    ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'San'],
-];
-
-lang === 'ru' ? console.log(langArray[0].join(', ')) : console.log(langArray[1].join(', '));
-
-
-
-//У нас есть переменная namePerson. Если значение этой переменной “Артем” то вывести в консоль “директор”, если значение “Александр” то вывести в консоль “преподаватель”, с любым другим значением вывести в консоль “студент”. Решить задачу с помощью нескольких тернарных операторов, без использования if или switch
-
-let namePerson = 'Артём';
-
-let message = (namePerson === 'Артём') ? 'директор' :
-    (namePerson === 'Александр') ? 'преподаватель' :
-        'студент';
-
-console.log(message);
+myFunc(2);
+myFunc(null);
+myFunc('Алена');
+myFunc('Эта строка содержит более 30 знаков');
