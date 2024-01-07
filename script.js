f0a438f1b010c4c4eaa87b99d48fa2111cab6538
Alena Nemtsova <jelenatbs@gmail.com>
2024-01-07 19:24:23 +0300

    add lesson07

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
 'use strict';
 
-//1) Записать в массив 7 любых многозначных чисел в виде строк. Вывести в консоль только те, что начинаются с цифры 2 или 4
-let arr = ['12345', '5555', '267887', '94838', '400', '1000', '22222'];
-
-for (let elem of arr) {
-    if (elem[0] == 2 || elem[0] == 4) {
-        console.log(elem);
-    }
-}
+const week = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+const showDays = document.querySelector('#days');
 
 
-//2) Вывести в столбик все простые числа от 1 до 100 (сделать при помощи цикла)
+const daysToNewLine = week.join('\n');
 
-for (let i = 2; i <= 100; i++) {
-    let flag = true;
-    for (let j = 2; j < i; j++) {
-        if (i % j === 0) {
-            flag = false;
-            continue;
-        }
-    }
-    if (flag === true) console.log(i, `Делители этого числа: 1 и ${i}`);
-}
+const satToItalic =
+    daysToNewLine.replace('Sat', '')
+
+showDays.append(daysToNewLine)
